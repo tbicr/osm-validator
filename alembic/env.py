dd363061fbe8ab9709386856c3fcff73ed80fd8c
Ramanchyk <a.ramanchyk@gmail.com>
2017-10-10 01:42:31 +0300

    edit env.py
    error
    
    """
    E:\PYTHON\OSM\osm-validator>alembic upgrade head
    usage: alembic [-h] [host_port]
    alembic: error: unrecognized arguments: head
    """

--- alembic/env.py
+++ alembic/env.py
@@ -1,7 +1,14 @@
 from __future__ import with_statement
 from alembic import context
+import os
 from sqlalchemy import engine_from_config, pool
 from logging.config import fileConfig
+
+import sys
+dirname = os.path.dirname(__file__)
+sys.path.append(os.path.abspath(os.path.join(dirname, '..')))
+
+import settings, main
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
@@ -14,13 +21,20 @@ fileConfig(config.config_file_name)
 # add your model's MetaData object here
 # for 'autogenerate' support
 # from myapp import mymodel
+app = main.app
 # target_metadata = mymodel.Base.metadata
-target_metadata = None
+# target_metadata = None
+target_metadata = app['db_declarative_base'].metadata
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
 # my_important_option = config.get_main_option("my_important_option")
 # ... etc.
+config.set_main_option('sqlalchemy.url', "postgresql://{}:{}@{}/{}".format(
+                           settings.DATABASE['user'],
+                           settings.DATABASE['password'],
+                           settings.DATABASE['host'],
+                           settings.DATABASE['database']))
 
 
 def run_migrations_offline():
